ad71b58cc0aca32a8e152030cde5361ef5e678f9
MacBook Pro <macbook@MacBookui-MacBookPro.local>
2026-06-16 18:09:42 +0900

    Organize font directory and use NanumGothic-Regular.ttf

--- scripts/virtual_pad.gd
+++ scripts/virtual_pad.gd
@@ -1,9 +1,8 @@
 extends CanvasLayer
 
 func _ready():
-	# 모바일 환경(Android, iOS)이거나 개발용 디버그 빌드일 때만 터치 패드를 활성화합니다.
-	# (PC에서도 마우스 클릭으로 터치 패드 테스트를 할 수 있도록 디버그 모드에서는 항상 표시합니다.)
-	if OS.has_feature("mobile") or OS.is_debug_build():
+	# 모바일 환경, 모바일 웹(Android/iOS), 터치 사용 가능 기기, 또는 디버그 빌드인 경우 터치 패드를 활성화합니다.
+	if OS.has_feature("mobile") or OS.has_feature("web_android") or OS.has_feature("web_ios") or DisplayServer.is_touchscreen_available() or OS.is_debug_build():
 		visible = true
 	else:
 		visible = false
